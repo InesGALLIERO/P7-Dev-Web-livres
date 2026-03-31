34fd674ca77c63e9fd0c0f9395f51bf0aad63678
InesGALLIERO <104718756+InesGALLIERO@users.noreply.github.com>
2026-03-31 05:04:54 +0200

    partie4

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -2,21 +2,48 @@
 const Book = require('../models/book')
 
 exports.createBook = (req, res, next) => {
-    
-    delete req.body._id 
-    const book = new Book({
-      ...req.body
-    });
-    book.save()
-    .then(() => res.status(201).json({ message: 'livre enregistré'}))
-    .catch(error => res.status(400).json({error }));
+   const bookObject = JSON.parse(req.body.book);
+   delete bookObject._id;
+   delete bookObject._userId;
+   const book = new Book({
+       ...bookObject,
+       userId: req.auth.userId,
+       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+   });
+ 
+   book.save()
+   .then(() => { res.status(201).json({message: 'Objet enregistré !'})})
+   .catch(error => { res.status(400).json( { error })})
 };
 
+
+
+
 exports.modifyBook = (req, res, next) => {
-  Book.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id })
-  .then( () => res.status(200).json({ message: 'livre modifié'}) )
-  .catch(error => res.status(400).json({error }))
+
+  const bookObject = req.file ? {
+    ...JSON.parse(req.body.thing),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete bookObjet._useId;
+  Book.findOne({_id: req.params.id})
+  .then( (book) => {
+    if(book.userId != req.auth.userId){
+      res.status(401).json({ message : 'Not authorized'});
+    }else {
+      Book.updateOne({ _id: req.params.id}, { ...bookObject, _id: req.params.id})
+        .then(() => res.status(200).json({message : 'livre modifié!'}))
+        .catch(error => res.status(401).json({ error }));
+    }
+  })
+  .catch( (error) => {
+      res.status(400).json({error})
+  } )
 };
+
+
+
 
 exports.deleteBook = (req, res, next) => {
   Book.deleteOne({ _id: req.params.id })
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const auth = require ('../middleware/auth');
 const router = express.Router();
 
+const auth = require ('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 const stuffCtrl = require('../controllers/stuff')
 
 
-router.post('/', auth, stuffCtrl.createBook);
+router.get('/', auth, stuffCtrl.getAllBooks);
+router.post('/', multer, auth, stuffCtrl.createBook);
+router.get('/:id', auth, stuffCtrl.getOneBook);
 router.put('/:id', auth, stuffCtrl.modifyBook);
 router.delete('/:id', auth, stuffCtrl.deleteBook);
-router.get('/:id', auth, stuffCtrl.getOneBook);
-router.get('/', auth, stuffCtrl.getAllBooks);
 
 module.exports = router;
