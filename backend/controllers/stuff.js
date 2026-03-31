e614a25ea66ff477f579cf864067419ad91fda9f
InesGALLIERO <104718756+InesGALLIERO@users.noreply.github.com>
2026-03-31 05:31:01 +0200

    modification partie4

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,5 +1,6 @@
 //logique pour chaque fonction
 const Book = require('../models/book')
+const fs = require('fs');
 
 exports.createBook = (req, res, next) => {
    const bookObject = JSON.parse(req.body.book);
@@ -46,9 +47,23 @@ exports.modifyBook = (req, res, next) => {
 
 
 exports.deleteBook = (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-  .then( () => res.status(200).json({ message: 'livre supprimé'}) )
-  .catch(error => res.status(400).json({error }))
+  Book.findOne({_id: req.params.id})
+  
+  .then( (book) => {
+    if(book.userId != req.auth.userId){
+      res.status(401).json({ message : 'Not authorized'});
+    }else {
+      const filename = book.imageUrl.split('/image/') [1];
+       fs.unlink(`images/${filename}`, () => {
+                   Thing.deleteOne({_id: req.params.id})
+                       .then(() => { res.status(200).json({message: 'Objet supprimé !'})})
+                       .catch(error => res.status(401).json({ error }));
+               });
+    }
+  })
+  .catch( (error) => {
+      res.status(500).json({error})
+  } )
 };
 
 exports.getOneBook = (req, res, next) => {
